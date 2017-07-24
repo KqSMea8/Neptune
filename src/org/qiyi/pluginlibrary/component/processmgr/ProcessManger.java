7f184935b445586641023d245c51e2e113b7876e
yuanzeyao <yuanzeyao@qiyi.com>
2017-07-24 16:53:56 +0800

    修复插件中心分配进程错误的问题 版本:8.7.5
    Change-Id: Ifb9748da1170333ceb85ae268e8c186dc4b171a0

--- src/org/qiyi/pluginlibrary/component/processmgr/ProcessManger.java
+++ src/org/qiyi/pluginlibrary/component/processmgr/ProcessManger.java
@@ -17,15 +17,11 @@ import org.qiyi.pluginlibrary.utils.PluginDebugLog;
 
 public class ProcessManger {
     private static final String TAG = "ProcessManger";
-    private static final String HOST_PROCESS_NAME = "com.qiyi.video";
     public static interface IProcessSelecter{
-        public String selectProcess(String packageName);
-
         public int getProcessIndex(String processName);
     }
 
     private static IProcessSelecter mOutterSelecter;
-    private static IProcessSelecter mDefaultSelecter = new DefaultProcessSelecter();
 
     /**
      * 外面设置的进程选择器
@@ -35,83 +31,22 @@ public class ProcessManger {
         mOutterSelecter = mSelecter;
     }
 
-    /**
-     * 选择插件运行的进程名称,优先通过{@link #mDefaultSelecter}获取进程名称
-     * 如果没有获取到并且{@link #mOutterSelecter}不为空，那么通过{@link #mOutterSelecter}
-     * 获取
-     * @param packageName
-     *          需要查找运行进程名称的插件包名
-     * @return
-     *          找到返回插件运行进程的名称，没有找到返回Null
-     */
-    public static String selectProcess(String packageName){
-        if(TextUtils.isEmpty(packageName)){
-            return null;
-        }
-        String mProcessName = mDefaultSelecter.selectProcess(packageName);
-        if(mProcessName == null && mOutterSelecter != null){
-            mProcessName = mOutterSelecter.selectProcess(packageName);
-        }
-        return mProcessName;
-    }
 
     /**
      * 获取进程名称对应的index
      * @param processName
+     *          插件运行的进程名称
      * @return
+     *          插件的index
      */
     public static int getProcessIndex(String processName){
         if(TextUtils.isEmpty(processName)){
-            return -1;
+            return 1;
         }
-        int index = mDefaultSelecter.getProcessIndex(processName);
-        if(index== -1 && mOutterSelecter!= null){
-            index = mOutterSelecter.getProcessIndex(processName);
+        if(mOutterSelecter!= null){
+            int index = mOutterSelecter.getProcessIndex(processName);
+            return index;
         }
-        return index;
-    }
-
-
-    /**
-     * 默认的进程选择器，如果插件方有在Application组件中设置android:process 属性，那么默认选择器返回
-     * android:process的值，否则返回Null
-     */
-    private static class DefaultProcessSelecter implements IProcessSelecter{
-
-        @Override
-        public String selectProcess(String packageName) {
-            if(TextUtils.isEmpty(packageName)){
-                return null;
-            }
-            PluginLoadedApk mLoadedApk = PluginManager.getPluginLoadedApkByPkgName(packageName);
-            if(mLoadedApk != null){
-                TargetMapping mapping = mLoadedApk.getPluginMapping();
-                if(mapping != null){
-                    String processName = mapping.getProcessName();
-                    if(!TextUtils.isEmpty(processName)){
-                        PluginDebugLog.log(TAG,"DefaultProcessSelecter getProcessName:"+processName);
-                        return processName;
-                    }
-                }
-            }
-            return null;
-        }
-
-        @Override
-        public int getProcessIndex(String processName) {
-            if(processName != null){
-                if(HOST_PROCESS_NAME.equals(processName)){
-                    //主进程
-                    return 0;
-                }else if(processName.endsWith(":plugin1")){
-                    //:plugin 进程
-                    return 1;
-                }else if(processName.endsWith(":plugin2")){
-                    //:plugin2进程
-                    return 2;
-                }
-            }
-            return -1;
-        }
+        return 1;
     }
 }
